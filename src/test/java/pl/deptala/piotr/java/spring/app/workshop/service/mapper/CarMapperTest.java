c1e41ba888bb9e0a956c7adc77cac1d8983ac36a
Piotr <deptala.piotr@gmail.com>
2022-11-10 21:34:57 +0100

    unit Test for CarMapper methods

--- src/test/java/pl/deptala/piotr/java/spring/app/workshop/service/mapper/CarMapperTest.java
+++ src/test/java/pl/deptala/piotr/java/spring/app/workshop/service/mapper/CarMapperTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import pl.deptala.piotr.java.spring.app.workshop.repository.entity.CarEntity;
 import pl.deptala.piotr.java.spring.app.workshop.web.model.CarModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class CarMapperTest {
 
@@ -36,6 +39,29 @@ class CarMapperTest {
         // Then
         Assertions.assertNotNull(mappedModel, "mappedModel is NULL");
     }
+
+    @Test
+    void fromEntityList() {
+        // Given
+        List<CarEntity> carEntityList = new ArrayList<>();
+
+        // When
+        List<CarModel> carModelList = carMapper.fromList(carEntityList);
+
+        // Then
+        Assertions.assertNotNull(carModelList, "converted List is NULL");
+    }
+
+    @Test
+    void fromModelList1() {
+        // Given
+        List<CarModel> carModelList = new ArrayList<>();
+
+        // When
+        List<CarEntity> carEntityList = carMapper.fromList1(carModelList);
+
+        // Then
+        Assertions.assertNotNull(carEntityList, "carEntityList List is NULL");
+
+    }
 }
-// TODO: 03.11.2022
-// testy jednoskowe dla
