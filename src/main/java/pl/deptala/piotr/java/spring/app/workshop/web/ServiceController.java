c969cd52543c9a1ee82981ee3c48b71551ff59f9
Piotr <deptala.piotr@gmail.com>
2022-12-22 20:18:18 +0100

    create car services structure

---
+++ src/main/java/pl/deptala/piotr/java/spring/app/workshop/web/ServiceController.java
@@ -0,0 +1,11 @@
+package pl.deptala.piotr.java.spring.app.workshop.web;
+
+public class
+ServiceController {
+}
+
+// TODO: 21.12.2022
+// Dla modelu Service CRUD stworzyć wszystkie warstwy (Controller, Service, Mapper, Repository) analogicznie do modelu Car
+// Model Service reprezentuje usługe serwisowa np wymiana oleju
+// Dodać testy jednoskowe dla wszystkich warstw nalogicznie do modelu Car
+// Stworzyć pliki html z widokami dla usług serwisowych
---
+++ src/main/java/pl/deptala/piotr/java/spring/app/workshop/web/model/ServiceModel.java
@@ -0,0 +1,8 @@
+package pl.deptala.piotr.java.spring.app.workshop.web.model;
+
+public class ServiceModel {
+
+    private double price;
+    private String date;
+    private String name;
+}
