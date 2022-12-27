a429216c77774dceef2990c63e2978c161e300e5
deptala <deptala.piotr@gmail.com>
2022-12-27 11:37:14 +0100

    create business logic for service create method & test

--- src/main/java/pl/deptala/piotr/java/spring/app/workshop/service/ServiceService.java
+++ src/main/java/pl/deptala/piotr/java/spring/app/workshop/service/ServiceService.java
@@ -2,6 +2,9 @@ package pl.deptala.piotr.java.spring.app.workshop.service;
 
 import org.springframework.stereotype.Service;
 import pl.deptala.piotr.java.spring.app.workshop.repository.ServiceRepository;
+import pl.deptala.piotr.java.spring.app.workshop.repository.entity.ServiceEntity;
+import pl.deptala.piotr.java.spring.app.workshop.service.mapper.ServiceMapper;
+import pl.deptala.piotr.java.spring.app.workshop.web.model.ServiceModel;
 
 import java.util.logging.Logger;
 
@@ -10,15 +13,21 @@ public class ServiceService {
     private static final Logger LOGGER = Logger.getLogger(ServiceService.class.getName());
 
     private ServiceRepository serviceRepository;
+    private ServiceMapper serviceMapper;
 
-    public ServiceService(ServiceRepository serviceRepository) {
+    public ServiceService(ServiceRepository serviceRepository, ServiceMapper serviceMapper) {
         this.serviceRepository = serviceRepository;
+        this.serviceMapper = serviceMapper;
     }
 
     // C - create
-    public void create() {
-        LOGGER.info("create()");
-        LOGGER.info("create(...)");
+    public ServiceModel create(ServiceModel serviceModel) {
+        LOGGER.info("create(" + serviceModel + ")");
+        ServiceEntity serviceEntity = serviceMapper.from(serviceModel);
+        ServiceEntity savedServiceEntity = serviceRepository.save(serviceEntity);
+        ServiceModel mappedServiceModel = serviceMapper.from(savedServiceEntity);
+        LOGGER.info("create(...) = " + mappedServiceModel);
+        return mappedServiceModel;
     }
 
     // R - read
---
+++ src/test/java/pl/deptala/piotr/java/spring/app/workshop/service/ServiceServiceTest.java
@@ -0,0 +1,43 @@
+package pl.deptala.piotr.java.spring.app.workshop.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.deptala.piotr.java.spring.app.workshop.web.model.ServiceModel;
+
+@SpringBootTest
+class ServiceServiceTest {
+    @Autowired
+    private ServiceService serviceService;
+
+    @Test
+    void create() {
+        // Given
+        ServiceModel serviceModel= new ServiceModel();
+
+        // When
+        ServiceModel createdServiceModel = serviceService.create(serviceModel);
+
+        // Then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(createdServiceModel, "createdCarModel is NULL"));
+//                () -> Assertions.assertNotNull(createdServiceModel.getId(), "createdCarModel ID is NULL"));
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void list() {
+    }
+}
