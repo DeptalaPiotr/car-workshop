412126d49d143c9719b290abed8fb82df9f0d389
Piotr <deptala.piotr@gmail.com>
2022-12-27 10:40:54 +0100

    add LOGGER to controller and service class

--- src/main/java/pl/deptala/piotr/java/spring/app/workshop/web/ServiceController.java
+++ src/main/java/pl/deptala/piotr/java/spring/app/workshop/web/ServiceController.java
@@ -1,7 +1,41 @@
 package pl.deptala.piotr.java.spring.app.workshop.web;
 
+import pl.deptala.piotr.java.spring.app.workshop.service.CarService;
+
+import java.util.logging.Logger;
+
 public class
 ServiceController {
+    private static final Logger LOGGER = Logger.getLogger(ServiceController.class.getName());
+    // C - create
+    public void create() {
+        LOGGER.info("create()");
+        LOGGER.info("create(...)");
+    }
+
+    // R - read
+    public void read() {
+        LOGGER.info("read()");
+        LOGGER.info("read(...)");
+    }
+
+    // U - update
+    public void update() {
+        LOGGER.info("update()");
+        LOGGER.info("update(...)");
+    }
+
+    // D - delete
+    public void delete() {
+        LOGGER.info("delete()");
+        LOGGER.info("delete(...)");
+    }
+
+    // L - list
+    public void list() {
+        LOGGER.info("list()");
+        LOGGER.info("list(...)");
+    }
 
 }
 
