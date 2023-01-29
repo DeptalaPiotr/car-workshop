ca0e25463a5a312ddbd0fd0e0abc29436cd5d641
piotrdeptala <deptala.piotr@gmail.com>
2023-01-29 15:51:30 +0100

    created html view for user creation

--- src/main/java/pl/deptala/piotr/java/spring/app/workshop/web/UserController.java
+++ src/main/java/pl/deptala/piotr/java/spring/app/workshop/web/UserController.java
@@ -1,43 +1,62 @@
 package pl.deptala.piotr.java.spring.app.workshop.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.deptala.piotr.java.spring.app.workshop.service.UserService;
+import pl.deptala.piotr.java.spring.app.workshop.web.model.UserModel;
 
 import java.util.logging.Logger;
 
 @Controller
-@RequestMapping(value = "/cars/user")
+@RequestMapping(value = "/cars/users")
 public class UserController {
 
     private static final Logger LOGGER = Logger.getLogger(UserController.class.getName());
 
-      // C - create
-         public void create() {
-             LOGGER.info("create()");
-             LOGGER.info("create(...)");
-         }
+    private UserService userService;
 
-         // R - read
-         public void read() {
-             LOGGER.info("read()");
-             LOGGER.info("read(...)");
-         }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-         // U - update
-         public void update() {
-             LOGGER.info("update()");
-             LOGGER.info("update(...)");
-         }
+    // C - create
+    @GetMapping(value = "/create")
+    public String createView() {
+        LOGGER.info("create()");
+        return "user/create-user.html";
+    }
 
-         // D - delete
-         public void delete() {
-             LOGGER.info("delete()");
-             LOGGER.info("delete(...)");
-         }
+    @PostMapping()
+    public String create(UserModel userModel) {
+        LOGGER.info("create(" + userModel + ")");
+        UserModel createdUserModel = userService.create(userModel);
+        LOGGER.info("create(...) " + createdUserModel);
+        return "redirect:/cars";
+    }
 
-         // L - list
-         public void list() {
-             LOGGER.info("list()");
-             LOGGER.info("list(...)");
-         }
+    // R - read
+    public void read() {
+        LOGGER.info("read()");
+        LOGGER.info("read(...)");
+    }
+
+    // U - update
+    public void update() {
+        LOGGER.info("update()");
+        LOGGER.info("update(...)");
+    }
+
+    // D - delete
+    public void delete() {
+        LOGGER.info("delete()");
+        LOGGER.info("delete(...)");
+    }
+
+    // L - list
+    public void list() {
+        LOGGER.info("list()");
+        LOGGER.info("list(...)");
+    }
 }
